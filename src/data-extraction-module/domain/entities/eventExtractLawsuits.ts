ec50b953250c360f874bbb7e2d64efb5741695ac
José Nogueira <jose.nogueira@forumhub.com.br>
2025-01-24 07:56:55 -0300

    data extraction, change some ids definitions

--- src/data-extraction-module/domain/entities/eventExtractLawsuits.ts
+++ src/data-extraction-module/domain/entities/eventExtractLawsuits.ts
@@ -5,18 +5,18 @@ export enum EventExtractLawsuitsStatus {
 }
 
 export class EventExtractLawsuits {
-  public id: string;
+	public id: string;
 
-  constructor(
-    public requestedCnpj: string, // original requested cnpj
-    public searchedCnpj: string, // cleared cnpj searched
-    public status: EventExtractLawsuitsStatus,
-    public startDate: Date,
-    public endDate?: Date,
-    public totalPages?: number,
-    public pagesDownloaded?: number,
-    public nextPageUrl?: string | null,
-  ) {
-    this.id = `${startDate.toISOString()}`;
-  };
+	constructor(
+		public requestedCnpj: string, // original requested cnpj
+		public searchedCnpj: string, // cleared cnpj searched
+		public status = EventExtractLawsuitsStatus.PENDING,
+		public startDate = new Date(),
+		public endDate?: Date,
+		public totalPages?: number,
+		public pagesDownloaded?: number,
+		public nextPageUrl?: string | null
+	) {
+		this.id = `${searchedCnpj}#${startDate.toISOString()}`;
+	}
 }
