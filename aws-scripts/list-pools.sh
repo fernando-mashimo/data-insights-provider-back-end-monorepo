f3dbf84357bac2c706147522773bcf799eb0f484
José Nogueira <jose.nogueira@usedeltaai.com>
2025-02-13 11:27:02 -0300

    added more helpful aws scripts

---
+++ aws-scripts/list-pools.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+CREDENTIALS_PROFILE="delta-prod"
+
+# List all user pools in the account
+aws cognito-idp list-user-pools \
+  --max-results 60 \
+  --query 'UserPools[*].{
+    Id: Id,
+    Name: Name,
+    Status: Status
+  }' \
+  --output table \
+  --profile $CREDENTIALS_PROFILE
